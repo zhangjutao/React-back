8da319f28422cb7d73b8006c3d8445e3ee972b2b
zhangjt <862517061@qq.com>
2019-06-30 17:53:38 +0800

    antd引入及按需加载

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react';
+import {Button,message} from 'antd';
+class App extends Component{
 
-class App extends Component() {
+  handleClick = () => {
+    message.success("成功")
+  }
   render(){
     return (
       <div className="App">
         APP
+        <Button type="primary" onClick=
+        {this.handleClick}>Primary</Button>
       </div>
     );
   }
