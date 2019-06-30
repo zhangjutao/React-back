de791447aaa83db835ce8f420b3ee0e35d2eb57c
zhangjt <862517061@qq.com>
2019-06-30 21:04:09 +0800

    路由模块及组件

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -1,17 +1,18 @@
 import React,{Component} from 'react';
-import {Button,message} from 'antd';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from './views/login/login';
+import Admin from './views/admin/admin';
+
 class App extends Component{
 
-  handleClick = () => {
-    message.success("成功")
-  }
   render(){
     return (
-      <div className="App">
-        APP
-        <Button type="primary" onClick=
-        {this.handleClick}>Primary</Button>
-      </div>
+      <BrowserRouter>
+        <Switch>{/*只匹配其中一个路由*/}
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
